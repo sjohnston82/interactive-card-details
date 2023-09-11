0b2fa1554a8cac98e6b05f80256a2ebdeecc0adf
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2023-09-11 06:36:02 -0400

    added form context

--- src/app/context/FormContext.tsx
+++ src/app/context/FormContext.tsx
@@ -11,8 +11,63 @@ import React, {
 type FormContextType = {
   name: string;
   setName: React.Dispatch<SetStateAction<string>>;
-  cardNumber: number;
-  setCardNumber: React.Dispatch<SetStateAction<number>>;
-  setNumber: React.Dispatch<SetStateAction<number>>;
+  cardNumber: string;
+  setCardNumber: React.Dispatch<SetStateAction<string>>;
+
   expMonth: number;
+  setExpMonth: React.Dispatch<SetStateAction<number>>;
+  expYear: number;
+  setExpYear: React.Dispatch<SetStateAction<number>>;
+  cv2: number;
+  setCv2: React.Dispatch<SetStateAction<number>>;
+  confirmed: boolean;
+  setConfirmed: React.Dispatch<SetStateAction<boolean>>;
+};
+
+type FormContextProviderProps = {
+  children: React.ReactNode;
+};
+
+export const FormContext = createContext<FormContextType>(
+  null as unknown as FormContextType
+);
+
+const FormContextProvider = ({ children }: FormContextProviderProps) => {
+  const [name, setName] = useState("");
+  const [cardNumber, setCardNumber] = useState("0000 0000 0000 0000");
+  const [expMonth, setExpMonth] = useState(0o0);
+  const [expYear, setExpYear] = useState(0o0);
+  const [cv2, setCv2] = useState(0o00);
+  const [confirmed, setConfirmed] = useState(false);
+
+  return (
+    <FormContext.Provider
+      value={{
+        name,
+        setName,
+        cardNumber,
+        setCardNumber,
+        expMonth,
+        setExpMonth,
+        expYear,
+        setExpYear,
+        cv2,
+        setCv2,
+        confirmed,
+        setConfirmed,
+      }}
+    >
+      {children}
+    </FormContext.Provider>
+  );
+};
+
+export function useFormContext() {
+  const context = useContext(FormContext);
+  if (!context) {
+    throw new Error("useFormContext must be used within a FormContextProvider");
+  }
+  return context;
 }
+
+export default FormContextProvider;
