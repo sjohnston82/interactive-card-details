bf0cec8444abc3d1a1299244d40643fc8de3435e
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2023-09-10 20:46:57 -0400

    redoing tailwind cfg and adding card details

--- src/app/_components/CardFront.tsx
+++ src/app/_components/CardFront.tsx
@@ -7,11 +7,20 @@ const CardFront = () => {
     <div className="w-3/4 absolute top-[123px] left-6">
       <div className="">
         <Image src={CardFrontImage} alt="Card front" />
-        <div className="flex absolute top-4 left-4 items-center gap-2"><span className="rounded-full bg-white w-7 h-7  "></span><span className="rounded-full border border-light-grayish-violet w-3 h-3"></span></div>
-        
+        <div className="flex absolute top-4 left-4 items-center gap-2">
+          <span className="rounded-full bg-white w-7 h-7  "></span>
+          <span className="rounded-full border border-light-grayish-violet w-3 h-3"></span>
+        </div>
+        <div className="absolute text-light-grayish-violet flex justify-center top-20 tracking-widest left-5">
+          0000 0000 0000 0000
+        </div>
+        <div className="flex absolute justify-between mx-auto bottom-3 w-[84%] left-5 text-light-grayish-violet">
+          <p className="text-[10px] uppercase">Jane Appleseed</p>
+          <p className="text-[10px]">00/00</p>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default CardFront
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "gradient-start": "hsl(249, 99%, 64%)",
+        "gradient-end": "hsl(278, 94%, 30%)",
+        "red-error": "hsl(0, 100%, 66%)",
+        "light-grayish-violet": "hsl(270, 3%, 87%)",
+        "dark-grayish-violet": "hsl(279, 6%, 55%)",
+        "very-dark-violet": "hsl(278, 68%, 11%)",
+      },
+      fontSize: {
+        norm: "18px",
+      },
+      font: {
+        "space-grotesk": ["Space Grotesk"],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
